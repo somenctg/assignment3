246d6748903ba26d6da8c8b2d07a854bb8d9b61d
somenctg <somensingha06@gmail.com>
2021-01-22 18:53:44 +0600

    added

--- assignment.js
+++ assignment.js
@@ -1,20 +1,23 @@
 function kilometerToMeter(kilometer) {
-    var meter = kilometer/1000;
+    var meter = kilometer / 1000;
     return meter;
 }
 
 
-var friends = ["Rahul","Ayan","Subrata","Sakib","Saiful"];
-var longestName = friends[0];
+var friends = ["Rahul", "Ayan", "Subrata", "Sakib", "Saiful"];
+    var longestName = friends[0];
 
-function megaFriend(friends){
-    for(var i = 0; i<friends.length;i++){
+function megaFriend(friends) {
+    
+
+    for (var i = 0; i < friends.length; i++) {
         var element = friends[i];
-        if(element.length>longestName.length){
-         longestName=element;
+        if (element.length > longestName.length) {
+            longestName = element;
         }
     }
     return longestName;
 }
 var result = megaFriend(friends);
 console.log(result);
+
